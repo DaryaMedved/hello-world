6cd306eeda99e731fc7db5e5a3697e7c1560898f
DaryaMedved <medveddarya@yahoo.com>
2022-12-14 02:14:57 +0300

    hw2

---
+++ src/by/itacademy/hw2/Task5.java
@@ -0,0 +1,16 @@
+package by.itacademy.hw2;
+
+public class Task5 {
+    public static void main(String[] args) {
+        int a;
+        a = 8;
+        if (a%2==0) {
+            System.out.println(a + "четное число");
+        } else {
+            System.out.println(a+"нечетное число");
+
+        }
+
+    }
+
+}
