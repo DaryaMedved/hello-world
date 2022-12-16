6daeb0934e5b27d2c4b71ecd43bb0ff7cecddb5b
DaryaMedved <medveddarya@yahoo.com>
2022-12-16 23:15:40 +0300

    hw2

---
+++ src/by/itacademy/hw3/Task1.java
@@ -0,0 +1,13 @@
+package by.itacademy.hw3;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+        int b = 3;
+
+        if (b % 3 == 0) {
+            System.out.println(" 3 ");
+
+        }
+    }
+}
