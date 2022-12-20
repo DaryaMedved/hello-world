c803520e90589b7c906ce98199ec535611a7a579
DaryaMedved <medveddarya@yahoo.com>
2022-12-20 19:56:41 +0300

    hw4

---
+++ src/by/itacademy/hw4/Task8.java
@@ -0,0 +1,23 @@
+package by.itacademy.hw4;
+import java.util.Arrays;
+public class Task8 {
+    public static void main(String[] args) {
+        int[] mas = {66, 9, 44, 36, 8};
+
+        boolean isSorted = false;
+        int buf;
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < mas.length - 1; i++) {
+                if (mas[i] > mas[i + 1]) {
+                    isSorted = false;
+
+                    buf = mas[i];
+                    mas[i] = mas[i + 1];
+                    mas[i + 1] = buf;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(mas));
+    }
+}
