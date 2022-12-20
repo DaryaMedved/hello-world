844ba9e8f923ae84d6597092dae7effed3a82469
DaryaMedved <medveddarya@yahoo.com>
2022-12-20 21:09:49 +0300

    hw4

--- src/by/itacademy/hw3/Task1.java
+++ src/by/itacademy/hw3/Task1.java
@@ -1,13 +1,22 @@
 package by.itacademy.hw3;
-
+import java.util.Scanner;
 public class Task1 {
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        Scanner in = new Scanner((System.in));
+        System.out.print("Введите число: ");
+        int input = in.nextInt();
 
-        int b = 3;
-
-        if (b % 3 == 0) {
-            System.out.println(" 3 ");
-
+        if (input > 0)
+        {
+            System.out.println("Положительное число");
+        }
+        else if (input < 0)
+        {
+            System.out.println("Отрицательное число");
+        }
+        else
+        {
+            System.out.println("число рано нулю");
         }
     }
 }
