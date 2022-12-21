1e605a6cb8518ff7cce35975ac0fa266c12a3d77
DaryaMedved <medveddarya@yahoo.com>
2022-12-21 23:24:52 +0300

    hw3

--- src/by/itacademy/hw3/Task7.java
+++ src/by/itacademy/hw3/Task7.java
@@ -1,15 +1,28 @@
 package by.itacademy.hw3;
-
 import java.util.Scanner;
 
 public class Task7 {
     public static void main(String[] args) {
-        System.out.println("Каков результат умножения одного числа на другое:");
-        Scanner sc = new Scanner(System.in);
-        int number = sc.nextInt();
 
-        number = 56 * 99 ;
-        System.out.println( "правильный ответ" + number);
+        Scanner scanner = new Scanner((System.in));
+        System.out.println("Введите первое число");
+        int a = scanner.nextInt();
+
+        System.out.println("Введите второе число");
+        int b = scanner.nextInt();
+
+        int myltiplication = a * b;
+
+        System.out.printf("Сколько будет %d умножить на %d?", a, b);
+        int userAnswer = scanner.nextInt();
+
+        if(userAnswer == myltiplication) {
+            System.out.println("Поздравляю! Вы знаете таблицу умножения");
+
+        } else {
+            System.out.printf("Неверный ответ!!! %d * %d = %d", a, b, myltiplication);
+        }
+
 
 
 
