6685d2c9aaf7c4033276e13b49dd3ce72f8ef153
DaryaMedved <medveddarya@yahoo.com>
2022-12-21 17:04:57 +0300

    hw3

--- src/by/itacademy/hw3/Task5.java
+++ src/by/itacademy/hw3/Task5.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class Task5 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        System.out.print("Введите четверть: ");
+        int input = sc.nextInt();
 
         int min = 10;
         if(min >= 0 && min <= 14){
