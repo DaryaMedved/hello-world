fdbe8a7b2e87c9a2ef76aef6481d2c1b75952882
DaryaMedved <medveddarya@yahoo.com>
2023-01-17 18:31:58 +0300

    hw8

---
+++ src/by/itacademy/hw8/CookingIt/CokingDemo.java
@@ -0,0 +1,25 @@
+package by.itacademy.hw8.CookingIt;
+
+public class CokingDemo {
+    public static void main(String[] args) {
+        Food food = new Food("tomato");
+        Food food1 = new Food("salad");
+        Food food2 = new Food("curdCheese");
+
+        Dish dish = new Dish("greekSalad");
+        Dish dish1 = new Dish("tomatoWithCheese");
+
+        Cooking drink = new Cooking() {
+            @Override
+            public void cook() {
+                System.out.println("Preparation of cold drink");
+            }
+        };
+        Cooking[] cookings = {food, food1, food2, dish, dish1, drink};
+
+        for (Cooking c : cookings) {
+            c.cook();
+        }
+        Cooking.cookFood(cookings);
+    }
+}
---
+++ src/by/itacademy/hw8/CookingIt/Food.java
@@ -0,0 +1,46 @@
+package by.itacademy.hw8.CookingIt;
+
+import java.util.Objects;
+
+public class Food implements Cooking {
+    private String name;
+
+    public Food(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    @Override
+    public void cook() {
+        System.out.println("Preparation food " + getName());
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Food food = (Food) o;
+        return Objects.equals(name, food.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Food{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
